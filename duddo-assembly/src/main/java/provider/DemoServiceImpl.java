f598762937907af4a3c6b386f006d8155ce37758
qiuwenxiang <qiuwenxiang>
2018-04-16 15:58:48 +0800

    duddo部分

--- duddo-assembly/src/main/java/provider/DemoServiceImpl.java
+++ duddo-assembly/src/main/java/provider/DemoServiceImpl.java
@@ -3,6 +3,7 @@ package provider;
 import consumer.DemoService;
 
 public class DemoServiceImpl implements DemoService {
+    @Override
     public String sayHello(String name) {
         return "Hello1 "+name;
     }
