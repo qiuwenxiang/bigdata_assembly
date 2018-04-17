5c605e0ee67c70e5f46d4e44452698d6e2604114
邱文翔 <274235712@qq.com>
2018-04-17 22:59:40 +0800

    director 高可用 整理

--- spark-streaming-assembly/src/main/scala/com/kylin/assembly/spark/streaming/ScrapyAnalyticsAlgo.scala
+++ spark-streaming-assembly/src/main/scala/com/kylin/assembly/spark/streaming/ScrapyAnalyticsAlgo.scala
@@ -4,12 +4,14 @@ package com.kylin.assembly.spark.streaming
 import com.alibaba.fastjson.{JSON, JSONObject}
 import com.kylin.assembly.common.GlobalParamValue
 import com.kylin.assembly.spark.streaming.constant.StreamConstant
+import kafka.api.{OffsetRequest, PartitionOffsetRequestInfo, TopicMetadataRequest}
 import org.apache.spark.SparkConf
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.dstream.InputDStream
-import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaUtils}
+import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaUtils, OffsetRange}
 import org.apache.spark.streaming.{Duration, StreamingContext}
 import kafka.common.TopicAndPartition
+import kafka.consumer.SimpleConsumer
 import kafka.message.MessageAndMetadata
 import kafka.serializer.StringDecoder
 import kafka.utils.{ZKGroupTopicDirs, ZKStringSerializer, ZkUtils}
@@ -67,11 +69,32 @@ object ScrapyAnalyticsAlgo {
       var kafkaStream : InputDStream[(String,String)] = null
       var fromOffsets : Map[TopicAndPartition,Long] = Map()
       if (children >0){
-        for (i <- 0 to (children -1)){
+        val getLeaderConsumer = new SimpleConsumer("liebao235.test.com", 6667, 10000, 10000, "OffsetLookup") // 第一个参数是 kafka broker 的host，第二个是 port
+        val req = new TopicMetadataRequest(List(topic), 0)
+        val res = getLeaderConsumer.send(req)
+        val topicMetaOption = res.topicsMetadata.headOption
+        // 将partitions 转为  partitionId->leader 映射，方便下一步获取有效offset
+        val partitions = topicMetaOption match {
+          case Some(tm) =>
+            tm.partitionsMetadata.map(pm => (pm.partitionId, pm.leader.get.host)).toMap[Int, String]  // 将结果转化为 partition -> leader 的映射关系
+          case None =>
+            Map[Int, String]()
+        }
+        for (i <- 0 until children){
           val partitionOffset = zkClient.readData[String](s"${topicDirs.consumerOffsetDir}/${i}")
           val tp = TopicAndPartition(topic,i)
-          fromOffsets += (tp -> partitionOffset.toLong)
-          LOGGER.info("@@@@@@ topic[" + topic + "] partition[" + i + "] offset[" + partitionOffset + "] @@@@@@")
+          val requestMin = OffsetRequest(Map(tp -> PartitionOffsetRequestInfo(OffsetRequest.EarliestTime, 1)))
+
+          val consumerMin = new SimpleConsumer(partitions(i), 6667, 10000, 10000, "getMinOffset")  //注意这里的 broker_host，因为这里会导致查询不到，解决方法在下面
+          val curOffsets = consumerMin.getOffsetsBefore(requestMin).partitionErrorAndOffsets(tp).offsets
+          var nextOffset = partitionOffset.toLong
+          LOGGER.info("topic[" + topic + "] partition[" + i + "] curOffsets.head[" + curOffsets.head + "] zk中offsets "+ partitionOffset)
+          if (curOffsets.length > 0 && nextOffset < curOffsets.head) {  // 通过比较从 kafka 上该 partition 的最小 offset 和 zk 上保存的 offset，进行选择
+            nextOffset = curOffsets.head
+          }
+          fromOffsets += (tp -> nextOffset)
+          /*fromOffsets += (tp -> partitionOffset.toLong)*/
+          LOGGER.info("最终消费 topic[" + topic + "] partition[" + i + "] offset[" + partitionOffset + "] @@@@@@")
         }
         val messageHandler = (mmd : MessageAndMetadata[String, String]) => (mmd.topic, mmd.message())
         kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder, (String, String)](ssc, kafkaParam, fromOffsets, messageHandler)
@@ -79,10 +102,17 @@ object ScrapyAnalyticsAlgo {
         kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParam, topics)
       }
 
-
-      var events = kafkaStream.persist(StorageLevel.MEMORY_AND_DISK)
+      var offsetRanges = Array[OffsetRange]()
+      var events = kafkaStream.transform{ rdd =>
+         offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+        for (o <- offsetRanges) {
+          LOGGER.info(s"topic  ${o.topic}  partition ${o.partition}  fromoffset ${o.fromOffset} 打算消费到  untiloffset ${o.untilOffset}")
+        }
+          rdd
+      }
+        .persist(StorageLevel.MEMORY_AND_DISK)
         .map(line =>{
-          try {
+          /*try {
             val data = JSON.parseObject(line._2)
             Some(data)
           } catch {
@@ -91,20 +121,22 @@ object ScrapyAnalyticsAlgo {
               ex.printStackTrace()
               None
             }
-          }
+          }*/
+          line
         }).filter(obj => obj != None)
         .foreachRDD(rdd =>{
-          val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+
           rdd.foreachPartition( partitionOfRecord =>{
             partitionOfRecord.foreach(pair => {
-              print(pair)
+              LOGGER.info("qiuwx"+pair)
             })
           })
 
+          // rdd 完成后，更新offsetRanges
           for (o <- offsetRanges) {
             val zkPath = s"${topicDirs.consumerOffsetDir}/${o.partition}"
-            ZkUtils.updatePersistentPath(zkClient, zkPath, o.fromOffset.toString)  //将该 partition 的 offset 保存到 zookeeper
-            LOGGER.info(s"@@@@@@ topic  ${o.topic}  partition ${o.partition}  fromoffset ${o.fromOffset}  untiloffset ${o.untilOffset} #######")
+            ZkUtils.updatePersistentPath(zkClient, zkPath, o.untilOffset.toString)  //将该 partition 的 offset 保存到 zookeeper
+            LOGGER.info(s"save topic  ${o.topic}  partition ${o.partition}  untiloffset ${o.untilOffset} ")
           }
 
         }
