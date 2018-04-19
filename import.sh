e7be4c9d3ddf1e7179776462f3ac57be5335c674
邱文翔 <274235712@qq.com>
2018-04-19 23:30:30 +0800

    director 高可用 整理

--- import.sh
+++ import.sh
@@ -2,8 +2,23 @@ mysql -uroot -p123456 << EOF
  use autoHome;
  set names utf8;
 source /opt/part_0.sql
+EOF
+echo "part_0_import end" &
+mysql -uroot -p123456 << EOF
+ use autoHome;
+ set names utf8;
 source /opt/part_1.sql
+EOF
+echo "part_1_import end" &
+mysql -uroot -p123456 << EOF
+ use autoHome;
+ set names utf8;
 source /opt/part_2.sql
+EOF
+echo "part_2_import end" &
+mysql -uroot -p123456 << EOF
+ use autoHome;
+ set names utf8;
 source /opt/part_3.sql
 EOF
-echo "import end"
+echo "part_3_import end" &
